96283f3d47f46dc3a08335a0596790d5018f3527
sharbanis <sharbani.patil@gmail.com>
2026-05-16 17:21:20 +0530

    chapter 1

--- chapter_01_basics/02_JS.js
+++ chapter_01_basics/02_JS.js
@@ -1,11 +1,11 @@
 let a= 10;
 console.log(a); // 10
 
-for (let a = 0; a < 10000; i++) {
+for (let a = 0; a < 10; a++) {
     console.log(a); 
     print(a); 
 }
 
 function print() {
-    console.log("Hello World"); 
+    console.log("Hello World");
 }
