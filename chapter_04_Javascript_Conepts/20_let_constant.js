dd6b142d7269274d43897f027cadb7171ae45bde
Developer <dev@example.com>
2026-07-13 19:42:12 +0530

    filename update to js

---
+++ chapter_04_Javascript_Conepts/20_let_constant.js
@@ -0,0 +1,12 @@
+console.log(c);
+console.log("Hei");
+console.log("Hei");
+console.log("Hei");
+console.log("Hei");
+console.log("Hei");
+
+const c = "pramod;"
+
+console.log(PI);
+
+const PI = 3.14; //ReferenceError: Cannot access 'PI' before initialization
---
+++ chapter_04_Javascript_Conepts/21_jr_QA.js
@@ -0,0 +1,16 @@
+console.log(API_END_APP_VWO_COM);
+console.log("dasda")
+if (true) {
+
+}
+
+const API_END_APP_VWO_COM = "https://app.vwo.com/login/api";
+// -> Output - ReferenceError: Cannot access 'API_END_APP_VWO_COM' before initialization
+
+//correct way 
+const API_END_APP_VWO_COM = "https://app.vwo.com/login/api";
+console.log(API_END_APP_VWO_COM);
+console.log("dasda")
+if (true) {
+
+}
